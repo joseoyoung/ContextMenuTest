63ef99d1a56543fdabf5688667aa4858dd4c8df5
joseoyoung <josy3465@naver.com>
2017-05-25 19:55:48 +0900

    #4 complete

--- app/src/main/java/kr/hs/emirim/joseoyoung/contextmenutest/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/joseoyoung/contextmenutest/MainActivity.java
@@ -1,20 +1,25 @@
 package kr.hs.emirim.joseoyoung.contextmenutest;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+    LinearLayout linear;
+    Button but2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button but1=(Button)findViewById(R.id.but1);
-        Button but2=(Button)findViewById(R.id.but2);
+        but2=(Button)findViewById(R.id.but2);
+        linear=(LinearLayout)findViewById(R.id.linear);
         registerForContextMenu(but1);
         registerForContextMenu(but2);
     }
@@ -25,11 +30,37 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater menuInflater=getMenuInflater();
         if(v.getId()==R.id.but1){
             menu.setHeaderTitle("배경색 변경");
-            //menu.setHeaderIcon(R.drawable.icon1); 나중에 아이콘 넣어보기
+            menu.setHeaderIcon(R.drawable.icon1); //나중에 아이콘 넣어보기
             menuInflater.inflate(R.menu.menu1,menu);
         }
 
         if(v.getId()==R.id.but2)
             menuInflater.inflate(R.menu.menu2, menu);
     }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        super.onContextItemSelected(item);
+        switch (item.getItemId()){
+            case R.id.menu1_item_red:
+                linear.setBackgroundColor(Color.RED);
+                return true;
+            case R.id.menu1_item_blue:
+                linear.setBackgroundColor(Color.BLUE);
+                return true;
+            case R.id.menu1_item_pink:
+                linear.setBackgroundColor(Color.rgb(255,178,217));
+                return true;
+            case R.id.menu2_item_rotation:
+                but2.setRotation(but2.getRotation()+90);
+                return true;
+            case R.id.menu2_item_size:
+                but2.setScaleX(1.5f);
+                but2.setScaleY(1.5f);
+                return true;
+        }
+
+        return false;
+    }
+//조서영 빨리 사진 지워라^^ 내가 안지울 꺼니까 너가 알아서 지워라 아니면 너 오늘 실내화 또 없어진다?ㅋㅋㅋ
 }
